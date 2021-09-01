86d4194f4fb816af18c7438ca3e79518455f45ca
苏 <enmoton@qq.com>
2021-09-01 23:06:43 +0800

    spark sql

--- readme.md
+++ readme.md
@@ -13,3 +13,13 @@ spark streaming 流式计算
 spark MLlib 机器学习
 
 spark GraphX 图形计算
+
+# Spark SQL
+
+## DataFrame
+
+分布式数据集，带有schema元数据，可以将DataFrame当作数据库的表来处理。
+
+## DataSet
+
+分布式数据集，是DataFrame的扩展。DataFrame = DataSet[Row]
---
+++ src/main/scala/org/example/sql/SparkSql.scala
@@ -0,0 +1,26 @@
+package org.example.sql
+
+import org.apache.spark.sql.SparkSession
+
+object SparkSql {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .master("local[*]")
+      .appName(this.getClass.getSimpleName)
+      .getOrCreate()
+
+    val users = spark.read.json("datas/user.json")
+    users.createOrReplaceTempView("user")
+    users.show()
+
+    val userRoles = spark.read.json("datas/user_role.json")
+    userRoles.createOrReplaceTempView("user_role")
+    userRoles.show()
+
+    spark.sql("select * from user join user_role on user.id = user_role.user_id").show()
+    users.join(userRoles).where("id = user_id").show()
+
+    spark.stop()
+  }
+}
