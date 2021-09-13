4ebc718b04743e57b7cced10b6d0dd59c40da060
苏 <enmoton@qq.com>
2021-09-13 21:37:23 +0800

    spark window function

---
+++ src/main/scala/org/example/sql/SparkWindowFunction.scala
@@ -0,0 +1,32 @@
+package org.example.sql
+
+object SparkWindowFunction {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkContext.spark
+    import spark.implicits._
+
+    val df = Seq(
+      (1, "2021-01-01", 1000),
+      (1, "2021-01-02", 1010),
+      (1, "2021-01-03", 1005),
+      (1, "2021-01-05", 999),
+      (2, "2021-01-01", 2000),
+      (2, "2021-01-02", 2100),
+      (2, "2021-01-03", 2200),
+      (2, "2021-01-06", 1900),
+    ).toDF("id", "trading_day", "close")
+
+    df.createOrReplaceTempView("stock_quote")
+
+    val dataFrame = spark.sql(
+      """
+        |select *, (close - pre_close) / close as yield
+        |from (
+        |         select *,
+        |                min(close) over (partition by id order by trading_day rows between 1 preceding and 1 preceding) as pre_close
+        |         from stock_quote
+        |     ) as quote
+        |""".stripMargin)
+    dataFrame.show()
+  }
+}
